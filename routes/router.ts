cfb5a493fdafdfdd5409d7f402cce89427ab63e2
Milhernandez08 <milton30choko@hotmail.com>
2019-09-19 01:39:17 -0500

    Rutas actualizadas y una de prueba

--- routes/router.ts
+++ routes/router.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
+import { app } from '../classes/archivos';
 
 const sesion = require('../classes/sesion');
-/*const pais = require('../classes/paises');
+const pais = require('../classes/paises');
 const estados = require('../classes/estados');
-const municipios = require('../classes/municipios');*/
+const municipios = require('../classes/municipios');
 const usuario = require('../classes/Usuario');
-/*const lote = require('../classes/Lote');
+const lote = require('../classes/Lote');
 const muestra = require('../classes/Muestra');
-const croma = require('../classes/Croma_NN');*/
+const croma = require('../classes/Croma_NN');
 
 const router = Router();
-/*
+
 router.get('/pais/todos', pais.todos);
 router.get('/pais/:id', pais.porId);
 
@@ -18,7 +19,7 @@ router.get('/estado/todos/:id_pais', estados.todos);
 router.get('/estado/:id', estados.porId);
 
 router.get('/municipios/todos/:id_estado', municipios.todos);
-router.get('/municipios/:id', municipios.porId);*/
+router.get('/municipios/:id', municipios.porId);
 
 /* USUARIO */
 router.post("/login", sesion.login);
@@ -33,6 +34,37 @@ router.get('/usuario/rol/:rol', usuario.porRol);  //Admin = 1, Experto = 2, U_Re
 router.get('/usuario/nombre-y-rol/:nombre/:rol', usuario.porNombreYRol);
 router.put('/usuario/:id', usuario.editar);
 router.delete('/usuario/:id', usuario.eliminar);
+
+/* LOTE */
+router.post("/lote/crear", lote.crear);
+router.get("/lote/todos", lote.todos);
+router.get("/lote/:id", lote.porId);
+router.put("/lote/:id", lote.editar);
+router.delete("/lote/:id", lote.eliminar);
+
+/* MUESTRA */
+router.post("/muestra/crear", muestra.crear);
+router.get("/muestra/todos", muestra.todos);
+router.get("/muestra/:id", muestra.porId);
+router.put("/muestra/:id", muestra.editar);
+router.delete("/muestra/:id", muestra.eliminar);
+
+/* CROMA_NN */
+router.post("/croma/crear", croma.crear);
+router.get("/croma/todos", croma.todos);
+router.get("/croma/:id", croma.porId);
+router.put("/croma/:id", croma.editar);
+router.delete("/croma/:id", croma.eliminar);
+
+/* SUBIR ARCHIVOS */
+app.post('/subir', (req, res) =>{
+    let EDFile = req.files.file
+    EDFile.mv(`../imagenes/${EDFile.name}`,err => {
+        if(err) return res.status(500).send({ message : err })
+
+        return res.status(200).send({ message : 'Imagen Guardad' })
+    });
+});
 
 
 export default router;
